4413c0b5580ef6a466d4f51bdfd54ff2ba43e5bc
Léa Boulvard <lea.boulvard@meteo.fr>
2025-03-27 04:24:03 +0100

    Ajout d'une image test

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Liste des lieux visités
 var points = [
-    { name: "Mexico City", lat: 19.4326, lng: -99.1332, img: "https://via.placeholder.com/150", desc: "La capitale vibrante du Mexique !" },
+    { name: "Mexico City", lat: 19.4326, lng: -99.1332, img: "images/mexico (1).jpg", desc: "La capitale vibrante du Mexique !" },
     { name: "Cancún", lat: 21.1619, lng: -86.8515, img: "https://via.placeholder.com/150", desc: "Plages paradisiaques et vie nocturne." },
 ];
 
