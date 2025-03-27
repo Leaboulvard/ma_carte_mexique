469416c76ab7f656c8698061a53167799f585c60
Léa Boulvard <lea.boulvard@meteo.fr>
2025-03-28 00:23:30 +0100

    boucle test des pop images unique et images multiples

--- script.js
+++ script.js
@@ -9,18 +9,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Liste des lieux visités
 var points = [
     { name: "Mexico City - Plaza Aviacion", lat: 19.42879, lng: -99.10653, img: "images/mexico-1.jpg", desc: "Petite place sympathique typique de quartier mexicain." },
-    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42123, lng: -99.18024, img: "images/mexico-2.jpg", desc: "Ce petit mammifère est un écureueil gris du mexique (Sciurus aureogaster, elle est indigène au lieux autrement dit elle est présente naturellement dans la région." },
+    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42123, lng: -99.18024, img: "images/mexico-2.jpg", desc: "Ce petit mammifère est un écureuil gris du Mexique (Sciurus aureogaster), indigène à la région." },
     { name: "Mexico City - Bosque de Chapultepec", lat: 19.42063, lng: -99.18388, img: ["images/mexico-3.jpg", "images/mexico-5.jpg"], desc: "Petite vue sur el lago menor (Lac mineur) du parc." },
     { name: "Mexico City - Bosque de Chapultepec", lat: 19.42879, lng: -99.10653, img: "images/mexico-4.jpg", desc: "Petite place sympathique typique de quartier mexicain !" },
 ];
 
- // Ajout des marqueurs
- //   points.forEach(point => {
- //       L.marker([point.lat, point.lng]).addTo(map)
- //           .bindPopup(`<b>${point.name}</b><br>${point.desc}<br><img src="${point.img}" width="150">`);
- //   });
-
-// Ajout des marqueurs
+// Ajout des marqueurs avec gestion des images (singulières et multiples)
 points.forEach(point => {
     let imagesHTML = "";  // Variable pour stocker le HTML des images
 
