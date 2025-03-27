61c65d5e2b4cb22d4232d89876f04b631d1059c6
Léa Boulvard <lea.boulvard@meteo.fr>
2025-03-28 00:50:48 +0100

    esthétique des images et descriptions

--- script.js
+++ script.js
@@ -8,30 +8,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Liste des lieux visités
 var points = [
-    { name: "Mexico City - Plaza Aviacion", lat: 19.42879, lng: -99.10653, img: "images/mexico-1.jpg", desc: "Petite place sympathique typique de quartier mexicain." },
-    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42123, lng: -99.18024, img: "images/mexico-2.jpg", desc: "Ce petit mammifère est un écureuil gris du Mexique <i>(Sciurus aureogaster)</i>." },
-    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42063, lng: -99.18388, img: ["images/mexico-3.jpg", "images/mexico-5.jpg"], desc: "Petite vue sur el lago menor <i>(Lac mineur)</i> du parc." },
-    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42879, lng: -99.10653, img: "images/mexico-4.jpg", desc: "Petite place sympathique typique de quartier mexicain." },
+    { name: "Mexico City <br> Plaza Aviacion", lat: 19.42879, lng: -99.10653, img: "images/mexico-1.jpg", desc: "Petite place sympathique typique de quartier mexicain." },
+    { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42123, lng: -99.18024, img: "images/mexico-2.jpg", desc: "Ce petit mammifère est un écureuil gris du Mexique <i>(Sciurus aureogaster)</i>." },
+    { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42063, lng: -99.18388, img: ["images/mexico-3.jpg", "images/mexico-5.jpg"], desc: "Petite vue sur el lago menor (Lac mineur) du parc." },
+    { name: "Mexico City - Bosque de Chapultepec", lat: 19.42879, lng: -99.10653, img: "images/mexico-4.jpg", desc: "Petite place typique de quartier mexicain !" },
 ];
 
 // Ajout des marqueurs
 points.forEach(point => {
     let imagesHTML = "";  // Variable pour stocker les images
-    
-    // Vérifie si "point.img" est un tableau ou une chaîne de caractères
+
+    // Vérifie si "point.img" est une seule image ou un tableau d'images
     if (Array.isArray(point.img)) {
+        // Si c'est un tableau, on boucle et ajoute chaque image
         point.img.forEach(img => {
-            imagesHTML += `<img src="${img}" width="150" style="margin: 5px;">`;
+            imagesHTML += `<img src="${img}" width="150" style="margin: 5px; display: inline-block;">`; // Utilisation de display: inline-block
         });
     } else {
-        imagesHTML = `<img src="${point.img}" width="150" style="margin: 5px;">`;
+        // Sinon, ajoute une seule image
+        imagesHTML = `<img src="${point.img}" width="150" style="margin: 5px; display: inline-block;">`; // Utilisation de display: inline-block
     }
 
-    // Met en italique tout texte entre parenthèses
-    let formattedDesc = point.desc.replace(/\((.*?)\)/g, "<i>($1)</i>");
-    
-    // Création du marqueur avec un retour à la ligne et des photos côte à côte
+    // Création du marqueur avec la description et les images dans la popup
     L.marker([point.lat, point.lng]).addTo(map)
-        .bindPopup(`<b>${point.name.split(" - ")[0]}</b><br>${point.name.split(" - ")[1]}<br>${formattedDesc}<br>
-        <div style="display: flex; gap: 10px;">${imagesHTML}</div>`);
+        .bindPopup(`<b>${point.name}</b><br>${point.desc}<br>${imagesHTML}`);
 });
