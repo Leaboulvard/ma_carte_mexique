8029eef24b477f40396d03bfd5248787e918d770
Léa Boulvard <lea.boulvard@meteo.fr>
2025-04-22 18:06:27 +0200

    modification ligne directrice et images

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var points = [
     { name: "Mexico City <br> Plaza Aviacion", lat: 19.42879, lng: -99.10653, img: "images/mexico-1.jpg", desc: "Petite place sympathique typique de quartier mexicain." },
     { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42123, lng: -99.18024, img: "images/mexico-2.jpg", desc: "Ce petit mammifère est un écureuil gris du Mexique <i>(Sciurus aureogaster)</i>." },
-    { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42063, lng: -99.18388, img: "images/mexico-3.jpg", desc: "Petite vue sur el Lago menor ou le Lac mineur en français du parc." },
-    { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42879, lng: -99.10653, img: "images/mexico-4.jpg", desc: "Petite place typique de quartier mexicain !" },
+    { name: "Mexico City <br> Bosque de Chapultepec", lat: 19.42063, lng: -99.18388, img: "images/mexico-3.jpg", desc: "Petite vue sur el Lago menor ou le Lac mineur en français du parc." }
 ];
 
 // Ajout des marqueurs
@@ -25,12 +24,16 @@ points.forEach((point) => {
         });
 });
 
-// Création d’une ligne en pointillés entre les lieux
-var latlngs = points.map(p => [p.lat, p.lng]);
+// Création d'une polyline directrice en pointillés pour représenter le parcours
+var latlngs = points.map(p => [p.lat, p.lng]);  // Tableau de coordonnées
 
-L.polyline(latlngs, {
-    color: 'blue',
+// Création de la ligne avec style pointillé
+var parcours = L.polyline(latlngs, {
+    color: 'black',
     weight: 3,
-    opacity: 0.7,
-    dashArray: '10,10' // ligne pointillée
+    dashArray: '5, 10',  // Style pointillé
+    opacity: 0.7
 }).addTo(map);
+
+// Zoom automatique pour englober toute la ligne
+map.fitBounds(parcours.getBounds());
